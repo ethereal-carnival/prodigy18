8957669e78dd601f5920d148f9f09cbc7412a2d4
wraith-of-peril <vyommaitreya@gmail.com>
2017-11-09 13:22:57 +0530

    Established basic environment

---
+++ index.php
@@ -0,0 +1,9 @@
+<html lang="en">
+    <head>
+        <title>Prodigy 18 - Coming soon</title>
+
+        <link href="assets/css/common.css" rel="stylesheet" />
+    </head>
+    <body>
+    </body>
+</html>
